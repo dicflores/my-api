9dc934c42ebf0e10623db0fc957592765c7933e6
Damian Flores <dicflores@gmail.com>
2020-01-13 01:11:04 -0300

    Add DateTimeFormat annotation for request param

--- src/main/java/me/dicflores/myapi/calendar/CalendarController.java
+++ src/main/java/me/dicflores/myapi/calendar/CalendarController.java
@@ -1,5 +1,6 @@
 package me.dicflores.myapi.calendar;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,8 +22,8 @@ public class CalendarController {
 
     @GetMapping
     public Collection<LocalDate> getAvailableDates(
-        @RequestParam Optional<LocalDate> arrival,
-        @RequestParam Optional<LocalDate> departure
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Optional<LocalDate> arrival,
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Optional<LocalDate> departure
     ) {
         LocalDate from = arrival.orElse(LocalDate.now());
         LocalDate to = departure.orElse(from.plusMonths(1));
