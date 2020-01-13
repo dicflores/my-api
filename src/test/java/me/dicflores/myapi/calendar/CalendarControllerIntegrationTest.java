6bb1d5530a61dc6d59487dba6a57a74d90c62721
Damian Flores <dicflores@gmail.com>
2020-01-13 16:02:12 -0300

    Add parallel request test for calendar

--- src/test/java/me/dicflores/myapi/calendar/CalendarControllerIntegrationTest.java
+++ src/test/java/me/dicflores/myapi/calendar/CalendarControllerIntegrationTest.java
@@ -1,7 +1,6 @@
 package me.dicflores.myapi.calendar;
 
 import me.dicflores.myapi.util.JacksonHelper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,16 +9,17 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.net.URL;
+import java.lang.reflect.Array;
 import java.time.LocalDate;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CalendarControllerIntegrationTest {
-    private URL base;
-
     @LocalServerPort
     private int port;
 
@@ -28,11 +28,6 @@ public class CalendarControllerIntegrationTest {
 
     @Autowired
     private JacksonHelper jacksonHelper;
-
-    @BeforeEach
-    public void setUp() throws Exception {
-        this.base = new URL("http://localhost:" + port + "/calendar");
-    }
 
     @Test
     public void whenNoDatesProvided_thenUseDefaultDateRange() throws Exception {
@@ -72,5 +67,40 @@ public class CalendarControllerIntegrationTest {
         String body = response.getBody();
         assertTrue(body.contains("BAD_REQUEST"));
         assertTrue(body.contains("Invalid date range for calendar"));
+    }
+
+    @Test
+    public void whenMultipleRequests_thenHandleAllInParallel() throws Exception {
+        LocalDate from = LocalDate.now();
+        LocalDate to = from.plusMonths(1);
+        String expectedContent = jacksonHelper.generateDateArray(from, to);
+
+        String urlString = String.format("http://localhost:%d/calendar", port);
+        long start = System.currentTimeMillis();
+        CompletableFuture<ResponseEntity<String>> [] requests = getMultipleRequests(urlString);
+        CompletableFuture.allOf(requests).join(); // Wait for all threads.
+        long end = System.currentTimeMillis();
+
+        assertTrue(end - start < 1000, "Should finish in less than a second.");
+
+        for (int i = 0; i < requests.length; i++) {
+            ResponseEntity<String> response = requests[i].get();
+
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+            assertEquals(expectedContent, response.getBody());
+        }
+    }
+
+    private CompletableFuture<ResponseEntity<String>> [] getMultipleRequests(String urlString) {
+        int n = 200;
+        // Define a fixed Thread Pool of n threads.
+        Executor executor = Executors.newFixedThreadPool(n);
+
+        // Construct n CompletableFutures.
+        CompletableFuture<ResponseEntity<String>> [] array = (CompletableFuture<ResponseEntity<String>> [])Array.newInstance(CompletableFuture.class, n);
+        for (int i = 0; i < n; i++) {
+            array[i] = CompletableFuture.supplyAsync(() -> template.getForEntity(urlString, String.class), executor);
+        }
+        return array;
     }
 }
