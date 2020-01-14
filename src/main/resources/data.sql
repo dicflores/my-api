d91d33dac095d9e22f39e78f6b0b5f0adb94d6c8
Damian Flores <dicflores@gmail.com>
2020-01-14 13:44:20 -0300

    Update some properties

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO BOOKING (ID, EMAIL, FULL_NAME, ARRIVAL_DATE, DEPARTURE_DATE) VALUES (-1, 'nobody@mail.com', 'nobody', '1970-01-01', '2020-12-31');
+INSERT INTO booking (id, email, full_name, arrival_date, departure_date) VALUES (-1, 'nobody@mail.com', 'nobody', '1970-01-01', '2020-12-31');
 
 --INSERT INTO Calendar(day, booking_Id) VALUES (CURDATE() + INTERVAL 1 DAY, -1);
